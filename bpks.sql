403f999e2f6d3582bf8a5780a892d6891e11999b
Matheus Henrique Custodio <143349401+Mathueshcustodio@users.noreply.github.com>
2023-11-17 11:44:14 -0300

    Add files via upload

---
+++ bpks.sql
@@ -0,0 +1,134 @@
+CREATE DATABASE  IF NOT EXISTS `academia` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `academia`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: academia
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `aluno`
+--
+
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aluno` (
+  `matricula` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `CPF` varchar(11) NOT NULL,
+  `telefone` varchar(11) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  PRIMARY KEY (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aluno`
+--
+
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `funcionarios`
+--
+
+DROP TABLE IF EXISTS `funcionarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `funcionarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `departamento` varchar(255) NOT NULL,
+  `salario` decimal(8,2) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionarios`
+--
+
+LOCK TABLES `funcionarios` WRITE;
+/*!40000 ALTER TABLE `funcionarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `funcionarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modalidades`
+--
+
+DROP TABLE IF EXISTS `modalidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `modalidades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `duracao` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modalidades`
+--
+
+LOCK TABLES `modalidades` WRITE;
+/*!40000 ALTER TABLE `modalidades` DISABLE KEYS */;
+/*!40000 ALTER TABLE `modalidades` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `telefone` varchar(11) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `cref` varchar(10) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-17 12:39:05
---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+
+import mysql.connector
+
+limpar_tela = lambda: os.system('cls')
+conexao = mysql.connector.connect(user='root', password='302302', host='localhost', database='academia')
+cursor = conexao.cursor()
+
+
+# Retorna o id da entidade criada
+def inserir_dados():
+    valores = ['nome', 'CPF', 'telefone', 'endereco']
+    for i in range(len(valores)):
+        valores[i] = input(f'Digite um valor para a coluna {valores[i]}:')
+    cursor.execute('INSERT INTO aluno (nome, CPF, telefone, endereco) VALUES (%s,%s,%s,%s)', tuple(valores))
+    conexao.commit()
+    print(f'O aluno com o ID {cursor.lastrowid} foi criado')
+
+
+def mostrar_alunos():
+    print('TABELA ALUNO')
+    print(f'MATRICULA|{"NOME":<30}|{"CPF":<11}|{"TELEFONE":<11}|ENDEREÇO')
+    cursor.execute('SELECT * FROM aluno')
+    for (matricula, nome, CPF, telefone, endereco) in cursor:
+        print(f'{matricula:<9}|{nome:<30}|{CPF:<11}|{telefone:<11}|{endereco}')
+
+
+def puxar_dados(matricula_query):
+    cursor.execute(f'SELECT * FROM aluno WHERE matricula = {matricula_query}')
+    return cursor.fetchall()[0]
+
+
+def atualizar_dados():
+    matricula = int(input('Digite a matricula de um Aluno: '))
+    aluno = puxar_dados(matricula)
+    nome = input('Digite o novo nome do aluno: ') or aluno[1]
+    CPF = input('Digite o novo CPF: ') or aluno[2]
+    telefone = input('Digite o novo telefone: ') or aluno[3]
+    endereco = input('Digite o novo endereço: ') or aluno[4]
+    cursor.execute('UPDATE aluno SET nome = %s, CPF = %s, telefone = %s, endereco = %s WHERE matricula = %s',
+                   (nome, CPF, telefone, endereco, matricula))
+    conexao.commit()
+
+
+def deletar_dados(matricula):
+    cursor.execute('DELETE FROM aluno WHERE matricula = %s', (matricula,))
+    conexao.commit()
+    print(f'Aluno com matricula {matricula} deletado com exito.')
+
+
+while True:
+    menu = int(input(
+        f'▄☺▄☺▄☺▄☺▄☺▄☺▄\n1 → Inserir Dados\n2 → Ler Dados\n3 → Atualizar Dados\n4 → Deletar Dados\n5 → Sair\nSelecione uma opção: '))
+    limpar_tela()
+    match menu:
+        case 1:
+            inserir_dados()
+        case 2:
+            mostrar_alunos()
+        case 3:
+            mostrar_alunos()
+            atualizar_dados()
+        case 4:
+            mostrar_alunos()
+            deletar_dados(int(input('Digite a matricula do aluno: ')))
+        case 5:
+            break
+
+conexao.close()
